5ad5acbe9aa7279ddd95e9faa7a68b8add33e86b
Walisson <walissonmariano.dev@gmail.com>
2025-05-03 10:04:08 -0300

    add icon menu

--- src/app/shared/components/ui/menu/index.tsx
+++ src/app/shared/components/ui/menu/index.tsx
@@ -1,17 +1,24 @@
-import { LinkMenu } from "../linkMenu"
+import { AiFillHome } from "react-icons/ai";
+import { AiFillCalendar } from "react-icons/ai";
+import { AiFillShop } from "react-icons/ai";
+import { AiOutlineTeam } from "react-icons/ai";
+import { AiFillProfile } from "react-icons/ai";
+import { AiFillProduct } from "react-icons/ai";
+import { Link } from "react-router";
+import { AiOutlinePoweroff } from "react-icons/ai";
 
 const menuItems = [
     {
         title: "MENU",
         items: [
             {
-                icon: "",
+                icon: <AiFillHome />,
                 label: "Home",
                 href: "/home",
                 visible: ["admin"],
             },
             {
-                icon: "",
+                icon: <AiFillCalendar />,
                 label: "Agenda",
                 href: "/agenda",
                 visible: ["admin"],
@@ -22,25 +29,25 @@ const menuItems = [
         title: "CADASTRO BASICO",
         items: [
             {
-                icon: "",
+                icon: <AiFillShop />,
                 label: "Estabelecimento",
                 href: "/estabelecimento",
                 visible: ["admin"],
             },
             {
-                icon: "",
+                icon: <AiOutlineTeam />,
                 label: "Profissionais",
                 href: "/profissionais",
                 visible: ["admin"],
             },
             {
-                icon: "",
+                icon: <AiFillProfile />,
                 label: "Categorias",
                 href: "/categoria",
                 visible: ["admin"],
             },
             {
-                icon: "",
+                icon: <AiFillProduct />,
                 label: "Produtos e Serviços",
                 href: "/produtos",
                 visible: ["admin"],
@@ -52,21 +59,37 @@ const menuItems = [
 export const Menu = () => {
     return(
         <div className="flex flex-col justify-between h-full mt-4 text-sm">
-            <div className="w-30 text-center">
+            <div className="w-20 text-center">
                 <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcR5U16C8yXgBpl7-Bc7Itjx3_LRl425zINA&s" alt="logo" />
             </div>
             <div>
                 {menuItems.map((i)=>(
                     <div className="flex flex-col gap-2" key={i.title}>
-                        <span className="hidden lg:block bg-amber-400 border rounded-sm p-2 text-gray-400 font-light my-4">{i.title}</span>
+                        
+                        <span className="hidden lg:block bg-amber-400 border rounded-sm border-none p-2 text-gray-600 my-4 font-bold">
+                            {i.title}
+                        </span>
                         {i.items.map(item=>(
-                            <LinkMenu to={item.href} text={item.label} />
+                            <>
+                            <div className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2 rounded-md">
+                                <label>{item.icon}</label>
+                                <Link  to={item.href}>
+                                    {item.label}
+                                </Link>
+                            </div>
+                                
+                            </>
                         ))}
                     </div>
                 ))}
             </div>
             <div className="mt-6">
-                <LinkMenu to="/" text="Sair" />
+                <div className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2 rounded-md">
+                    <label><AiOutlinePoweroff /></label>
+                    <Link className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2 rounded-md" to={"/"}>
+                        Sair
+                    </Link>
+                </div>
             </div>
         </div>
     )
